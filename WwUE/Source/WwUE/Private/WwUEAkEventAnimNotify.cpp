44c5fdba0ae3a792571de4d9e6e16c6bf4b8f0c7
Jamie Carnell <jc228467@falmouth.ac.uk>
2023-02-24 23:37:57 +0000

    funne unreal wwise testing project

---
+++ WwUE/Source/WwUE/Private/WwUEAkEventAnimNotify.cpp
@@ -0,0 +1,5 @@
+// gaming
+
+
+#include "WwUEAkEventAnimNotify.h"
+
---
+++ WwUE/Source/WwUE/Public/WwUEAkEventAnimNotify.h
@@ -0,0 +1,17 @@
+// gaming
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WwUEAnimNotify.h"
+#include "WwUEAkEventAnimNotify.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WWUE_API UWwUEAkEventAnimNotify : public UWwUEAnimNotify
+{
+	GENERATED_BODY()
+
+};
